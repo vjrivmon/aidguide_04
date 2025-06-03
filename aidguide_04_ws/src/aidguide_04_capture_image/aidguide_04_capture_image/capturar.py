1944afc727108927758629a82e88ac38ddf9662f
Vicente Rivas Monferrer <vjrivmon@epsg.upv.es>
2025-06-03 15:53:07 +0200

    Cambios Mimi |
    Cascade señales semáforo

--- aidguide_04_ws/src/aidguide_04_capture_image/aidguide_04_capture_image/capturar.py
+++ aidguide_04_ws/src/aidguide_04_capture_image/aidguide_04_capture_image/capturar.py
@@ -12,7 +12,7 @@ from rclpy.node import Node
 from rclpy.qos import ReliabilityPolicy, QoSProfile 
 from datetime import datetime
 
-# Clase que convierte imágenes de ROS2 a OpenCV y detecta coches y caras con un clasificador Haar
+# Clase que convierte imágenes de ROS2 a OpenCV y detecta objetos (coches, caras, señales stop, peatones y semáforos) con clasificadores Haar
 class Ros2OpenCVImageConverter(Node):   
 
     def __init__(self):
@@ -42,6 +42,36 @@ class Ros2OpenCVImageConverter(Node):
         self.face_cascade = cv2.CascadeClassifier(face_cascade_path)
         if self.face_cascade.empty():
             self.get_logger().error("Error: No se pudo cargar el clasificador de caras.")
+            return
+
+
+        # Clasificador para stop
+        stop_cascade_path = os.path.join(package_share_directory, 'clasificadores', 'cascade_stop_3_15.xml')
+        self.stop_cascade= cv2.CascadeClassifier(stop_cascade_path)
+        if self.stop_cascade.empty():
+            self.get_logger().error("Error: No se pudo cargar el clasificador de stops.")
+            return
+        
+        # Clasificador para pesh
+        pesh_cascade_path = os.path.join(package_share_directory, 'clasificadores', 'cascade_pesh_lbp14.xml')
+        self.pesh_cascade = cv2.CascadeClassifier(pesh_cascade_path)
+        if self.pesh_cascade.empty():
+            self.get_logger().error("Error: No se pudo cargar el clasificador de semáforos.")
+            return
+        
+        # Clasificador para semáforos
+        z_cascade_path = os.path.join(package_share_directory, 'clasificadores', 'cascade_zepr_lbp13.xml')
+        self.z_cascade = cv2.CascadeClassifier(z_cascade_path)
+        if self.z_cascade.empty():
+            self.get_logger().error("Error: No se pudo cargar el clasificador de semáforos.")
+            return
+        
+
+        # Clasificador para semáforos
+        traffic_light_cascade_path = os.path.join(package_share_directory, 'clasificadores', 'TrafficLight_HAAR_16Stages.xml')
+        self.traffic_light_cascade = cv2.CascadeClassifier(traffic_light_cascade_path)
+        if self.traffic_light_cascade.empty():
+            self.get_logger().error("Error: No se pudo cargar el clasificador de semáforos.")
             return
 
         # Carpeta donde se guardarán las imágenes
@@ -78,6 +108,30 @@ class Ros2OpenCVImageConverter(Node):
                 minSize=(30, 30)
             )
             
+            # Detectar señales de stop
+            stops = self.stop_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30)
+            )
+            
+            # Detectar peatones
+            peshes = self.pesh_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30)
+            )
+            
+            # Detectar semáforos
+            traffic_lights = self.traffic_light_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30)
+            )
+            
             # Dibujar rectángulos alrededor de los coches (azul)
             for (x, y, w, h) in cars:
                 cv2.rectangle(cv_image, (x, y), (x+w, y+h), (255, 0, 0), 2)
@@ -88,9 +142,24 @@ class Ros2OpenCVImageConverter(Node):
                 cv2.rectangle(cv_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
                 cv2.putText(cv_image, 'Cara', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
             
+            # Dibujar rectángulos alrededor de las señales de stop (rojo)
+            for (x, y, w, h) in stops:
+                cv2.rectangle(cv_image, (x, y), (x+w, y+h), (0, 0, 255), 2)
+                cv2.putText(cv_image, 'Stop', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            
+            # Dibujar rectángulos alrededor de los peatones (amarillo)
+            for (x, y, w, h) in peshes:
+                cv2.rectangle(cv_image, (x, y), (x+w, y+h), (0, 255, 255), 2)
+                cv2.putText(cv_image, 'Peatón', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 255), 2)
+            
+            # Dibujar rectángulos alrededor de los semáforos (morado)
+            for (x, y, w, h) in traffic_lights:
+                cv2.rectangle(cv_image, (x, y), (x+w, y+h), (255, 0, 255), 2)
+                cv2.putText(cv_image, 'Semáforo', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 255), 2)
+            
             # Guardar imagen si se detectó algo y han pasado suficientes segundos
             current_time = time.time()
-            if (len(cars) > 0 or len(faces) > 0) and current_time - self.last_capture_time >= self.capture_interval:
+            if (len(cars) > 0 or len(faces) > 0 or len(stops) > 0 or len(peshes) > 0 or len(traffic_lights) > 0) and current_time - self.last_capture_time >= self.capture_interval:
                 timestamp = time.strftime("%Y%m%d-%H%M%S")
                 filename = os.path.join(self.output_folder, f'deteccion_{timestamp}.jpg')
                 cv2.imwrite(filename, cv_image)
@@ -98,7 +167,7 @@ class Ros2OpenCVImageConverter(Node):
                 self.last_capture_time = current_time
             
             # Mostrar la imagen
-            cv2.imshow('Detector de Coches y Caras', cv_image)
+            cv2.imshow('Detector de Objetos', cv_image)
             cv2.waitKey(1)
             
         except Exception as e:
